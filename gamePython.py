7143cc32f19d649d91707b66a10b9384b9e8fd3f
willcutrim <37386568+willcutrim@users.noreply.github.com>
2018-09-15 13:16:41 -0300

    Add files via upload
    foi colocado um WHILE....

--- gamePython.py
+++ gamePython.py
@@ -1,12 +1,15 @@
 from random import randint
 from time import sleep
-print('==============  TENTE ADVINHAR O NUMERO!! ============== ')
-numero = int(input('coloque um numero de 1 a 5: '))
+print('==============  TENTE ADVINHAR O NUMERO!! ============== \n')
+numero = 0
 n1 = randint(1, 5)
-sleep(1)
-if numero == n1:
-   print('O numero foi {}:'.format(n1))
-   print ('===== PARABÉNS VOCÊ ACERTOU =====')
-elif numero != n1:
-   print('O numero foi {}'.format(n1))
-   print ('===== VOCÊ ERROU =====')
+while numero != n1:
+   numero = int(input('coloque um numero de 1 a 5: '))
+   sleep(1)
+   if numero == n1:
+      print('O numero foi {}:'.format(n1))
+      print ('\n===== PARABÉNS VOCÊ ACERTOU =====')
+   elif numero != n1:
+      print('Você errou! O Numero Escolhido Foi {}\n '.format(n1))
+      print ('===== TENTE NOVAMENTE =====')
+print('FIM DE JOGO')
